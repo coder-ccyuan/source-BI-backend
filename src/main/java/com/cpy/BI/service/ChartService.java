574339c58db2c8272f29e18388d3fc9f20e4da2d
cpy <66666666@qq.com>
2024-04-01 21:38:06 +0800

    project init

---
+++ src/main/java/com/cpy/BI/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.cpy.BI.service;
+
+import com.cpy.BI.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 成希德
+* @description 针对表【chart(用户)】的数据库操作Service
+* @createDate 2024-04-01 20:54:38
+*/
+public interface ChartService extends IService<Chart> {
+
+}
